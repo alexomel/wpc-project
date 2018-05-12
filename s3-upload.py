a8778c01afd12710b5c6bf259bea59e4a4a9251a
root <root@ip-10-0-0-34.ec2.internal>
2018-05-12 08:13:59 +0000

    r

--- s3-upload.py
+++ s3-upload.py
@@ -2,11 +2,11 @@ import boto3
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('alexo-test-wpc')
-my_file=open('to_be_uploaded.txt', 'w+')
+my_file=open('file.txt', 'w+')
 my_file.write('test content')
 my_file.close()
 
 bucket.put_object(
-        Key='test.txt',
-        Body=open('./to_be_uploaded.txt', 'rb')
+        Key='file.txt',
+        Body=open('./file.txt', 'rb')
 )
