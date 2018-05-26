df5f89a183a651337832fb2b3ace9506b6e9dc15
root <root@ip-10-0-0-34.ec2.internal>
2018-05-26 13:06:37 +0000

    ete

--- web/app.py
+++ web/app.py
@@ -4,25 +4,32 @@ app = Flask(__name__)
 import boto3, os
 from media.s3_storage import S3MediaStorage
 
-
 s3 = boto3.resource('s3')
 storage = S3MediaStorage(s3, os.getenv('APP_BUCKET_NAME'))
 
 @app.route("/")
 def hello():
-   return "T_T"
+  return "It works Hurray!!!!"
+
 @app.route("/order-animation")
 def order_animation():
   return render_template(
-  "order.html",
-  invitation="Welcome at ani creator")
+    "order.html",
+    invitation="Welcome at ani creatior"
+  )
 
 @app.route("/upload", methods=["POST"])
-def upload():
+def handle_upload():
   if 'my_file' not in request.files:
-    return ":("
+    return "something is not yes!!!"
   my_file = request.files['my_file']
-  storage.save(path="ab/bc/photo", file_to_be_uploaded=my_file)
+  storage.save(
+   path="ab/bc/photo",
+   file_to_be_uploaded=my_file
+  )
+  
+  return "OK"
 
-if __name__ =='__main__':
+
+if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
